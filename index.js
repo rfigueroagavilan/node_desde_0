098bdbe57f496f57870362185cfe47677018742d
rfigueroagavilan <rfigueroagavilan@gmail.com>
2024-03-26 22:44:02 -0300

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const os = require("node:os");
+
 console.log("hola mundo");
 console.time("inicio");
 
 console.error("error");
 
 console.log([1, 2, 3, 4, 5]);
+
+
+console.log(os.uptime()/60/60);
